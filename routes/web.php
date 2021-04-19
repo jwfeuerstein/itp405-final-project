9a49d8f30d1c489dd27ef99a056da195250c8be7
Jason Feuerstein <jwfeuers@usc.edu>
2021-04-18 18:40:51 -0700

    Added Procfile

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\URL;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+
+if (env('APP_ENV') !== 'local') {
+    URL::forceScheme('https');
+}
 
 Route::get('/', function () {
     return view('welcome');
